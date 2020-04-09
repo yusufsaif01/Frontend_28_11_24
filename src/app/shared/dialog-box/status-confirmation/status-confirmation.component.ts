79daec99f2496e375cece631b86370148ec0355b
Pushpam Matah <pushpam.matah@4thpointer.com>
2020-04-09 12:59:38 +0530

    Added delete and status confirmation dialog box components

---
+++ src/app/shared/dialog-box/status-confirmation/status-confirmation.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-status-confirmation',
+  templateUrl: './status-confirmation.component.html',
+  styleUrls: ['./status-confirmation.component.scss']
+})
+export class StatusConfirmationComponent implements OnInit {
+  private message: string = 'Are you sure?';
+  private acceptText: string = 'Confirm';
+  private rejectText: string = 'Cancel';
+
+  constructor(
+    private dialogRef: MatDialogRef<any>,
+    @Inject(MAT_DIALOG_DATA) data: any
+  ) {
+    if (data.message) {
+      this.message = data.message;
+    }
+    if (data.acceptText) {
+      this.acceptText = data.acceptText;
+    }
+    if (data.rejectText) {
+      this.rejectText = data.rejectText;
+    }
+  }
+
+  ngOnInit() {}
+
+  event() {}
+}
