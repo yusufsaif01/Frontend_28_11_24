a5fb6ec64695c3b78b30ea0149d22fe5041b0404
Pushpam Matah <pushpam.matah@4thpointer.com>
2020-04-21 00:34:25 +0530

    left panel common component added

---
+++ src/app/shared/page-components/left-panel/left-panel.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-left-panel',
+  templateUrl: './left-panel.component.html',
+  styleUrls: ['./left-panel.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class LeftPanelComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
