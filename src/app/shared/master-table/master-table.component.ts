6f933a855e9346b8d331fde9b0bedb9788c291e8
amit4ps <amit.kumar@4thpointer.com>
2020-04-21 18:29:30 +0530

    Loaction List and add form

---
+++ src/app/shared/master-table/master-table.component.ts
@@ -0,0 +1,71 @@
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewChild,
+  ElementRef,
+  TemplateRef,
+  SimpleChanges
+} from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+
+@Component({
+  selector: 'app-master-table',
+  templateUrl: './master-table.component.html',
+  styleUrls: ['./master-table.component.scss']
+})
+export class MasterTableComponent implements OnInit {
+  @Input() tableConfig: any = {};
+  @Input() TableActions: TemplateRef<any>;
+  @Input() NumberColumn: boolean = false;
+  @Input() rows = new MatTableDataSource([]);
+  // dataSource = new MatTableDataSource<any>();
+  // rows = new MatTableDataSource([
+  //   {
+  //     serialNo: '1',
+  //     state: 'Uttar Pradesh',
+  //   },
+  //   {
+  //     serialNo: '2',
+  //     state: 'Kolkata',
+  //   },
+  //   {
+  //     serialNo: '3',
+  //     state: 'Assam',
+  //   },
+  //   {
+  //     serialNo: '4',
+  //     state: 'Kerala',
+  //   },
+
+  // ]);
+  public columns: string[] = [];
+
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild('nameRef', { static: true }) ElementnameRef: ElementRef;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.columns = this.tableConfig.allowedColumns;
+    if (this.TableActions) {
+      this.columns = this.columns.concat('action');
+    }
+    // if (this.NumberColumn) {
+    //   this.columns = ['sno'].concat(this.columns);
+    // }
+
+    // this.dataSource.paginator = this.paginator;
+  }
+
+  applyFilter(filterValue: string) {
+    this.rows.filter = filterValue.trim().toLowerCase();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.rows.sort = this.sort;
+  }
+}
