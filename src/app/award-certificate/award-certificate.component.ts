08cc3e7a13bc37bedd9b50afdcab823959082858
samyakjain12 <samyak.jain@4thpointer.com>
2020-04-22 12:59:48 +0530

    added pagination

--- src/app/award-certificate/award-certificate.component.ts
+++ src/app/award-certificate/award-certificate.component.ts
@@ -18,9 +18,10 @@ import { environment } from '../../environments/environment';
 export class AwardCertificateComponent implements OnInit {
   public tableConfig: AwardCertificateTableConfig = new AwardCertificateTableConfig();
   public dataSource = new MatTableDataSource([]);
-  pageSize: number = 20;
+  pageSize: number = 10;
   environment = environment;
   player_type: string;
+  award_count: number;
 
   panelOptions: object = {
     bio: true,
@@ -57,16 +58,26 @@ export class AwardCertificateComponent implements OnInit {
     this.player_type = value;
   }
 
+  updatePage(event: any) {
+    this.getAwardsList(this.pageSize, event.selectedPage);
+  }
+
   getAwardsList(page_size: number, page_no: number) {
     this.awardCertificateService
       .getAwardsList({ page_size, page_no })
       .subscribe(response => {
         let records = response.data.records;
         for (let i = 0; i < records.length; i++) {
-          records[i]['serialnumber'] = i + 1;
+          if (page_no > 1) {
+            records[i]['serialnumber'] =
+              i + 1 + page_size * page_no - page_size;
+          } else {
+            records[i]['serialnumber'] = i + 1;
+          }
           records[i]['media'] = environment.mediaUrl + records[i]['media'];
         }
         this.dataSource = new MatTableDataSource(records);
+        this.award_count = response.data.total;
       });
   }
 
