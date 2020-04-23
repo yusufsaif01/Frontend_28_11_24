3e2d480c0022fb11280b9a531cdfa756d1d11346
Rishabh Sharma <rishabh.sharma@4thpointer.com>
2020-04-24 00:27:26 +0530

    edit table (states)

---
+++ src/app/admin/masterdata/manage-location/manage-state/manage-state-service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { CredentialsService } from '@app/core';
+
+const routes = {
+  updateState: (state_id: any, country_id: any, data: any) =>
+    `/master/state/${country_id}/${state_id}`
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StateService {
+  constructor(
+    private httpClient: HttpClient,
+    private credentialsService: CredentialsService
+  ) {}
+
+  updateState(state_id: any, country_id: any, data: any): Observable<any> {
+    let token = this.credentialsService.isAuthenticated()
+      ? this.credentialsService.credentials['data']['token']
+      : '';
+    let httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + token
+      })
+    };
+    return this.httpClient.put<any>(
+      routes.updateState(state_id, country_id, data),
+      data,
+      httpOptions
+    );
+  }
+}
+
+// addState(context: AddStateContext): Observable<AddStateResponseContext> {
+//     let token = this.credentialsService.isAuthenticated()
+//       ? this.credentialsService.credentials['data']['token']
+//       : '';
+//     let httpOptions = {
+//       headers: new HttpHeaders({
+//         'Content-Type': 'application/json',
+//         Authorization: 'Bearer ' + token
+//       })
+//     };
+//     // let params = '/';
+//     // if (context['country_id']) {
+//     //   params += `${context['country_id']}`;
+//     // }
+//     return this.httpClient.post<AddStateResponseContext>(
+//       routes.addState(context),
+//       context,
+//       httpOptions
+//     );
+//   }
