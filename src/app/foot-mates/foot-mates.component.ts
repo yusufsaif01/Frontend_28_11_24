3dbfa770da031119bcd8ef998b1a9f68b42d773d
samyakjain12 <samyak.jain@4thpointer.com>
2020-05-12 14:54:03 +0530

    added service,html,ts part

--- src/app/foot-mates/foot-mates.component.ts
+++ src/app/foot-mates/foot-mates.component.ts
@@ -1,6 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { MutualFootmateComponent } from '@app/foot-request/mutual-footmate/mutual-footmate.component';
+import { FootMatesService } from './foot-mates.service';
+import { environment } from '../../environments/environment';
+
+interface FootMatesContext {
+  name: string;
+  position: string;
+  player_type: string;
+  avatar: string;
+  user_id: string;
+  mutuals: number;
+}
 
 @Component({
   selector: 'app-footmates',
@@ -26,8 +37,16 @@ export class FootMatesComponent implements OnInit {
     player_type: true,
     follows_buttons: false
   };
-
-  constructor(public dialog: MatDialog) {}
+  foot_request_count = 0;
+  foot_mate_count = 0;
+  pageSize: number = 20;
+  show_count: number = 0;
+  total_count: number = 0;
+  footMatesList: FootMatesContext[] = [];
+  constructor(
+    public dialog: MatDialog,
+    private footMatesService: FootMatesService
+  ) {}
   // MatualFootmates
   openDialog(): void {
     const dialogRef = this.dialog.open(MutualFootmateComponent, {
@@ -36,5 +55,32 @@ export class FootMatesComponent implements OnInit {
     });
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getFootMateList(this.pageSize, 1);
+    this.connectionStats();
+  }
+  getFootMateList(page_size: number, page_no: number) {
+    this.footMatesService.getFootMateList({ page_size, page_no }).subscribe(
+      response => {
+        let records = response.data.records;
+        for (let i = 0; i < records.length; i++) {
+          records[i]['avatar'] = environment.mediaUrl + records[i]['avatar'];
+        }
+        this.footMatesList = records;
+        this.show_count = response.data.records.length;
+      },
+      error => {}
+    );
+  }
+
+  connectionStats() {
+    this.footMatesService.connectionStats().subscribe(
+      response => {
+        this.foot_mate_count = response.data.footmates;
+        this.foot_request_count = response.data.footmate_requests;
+        this.total_count = response.data.footmates;
+      },
+      error => {}
+    );
+  }
 }
