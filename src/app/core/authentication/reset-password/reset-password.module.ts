509216fe9f8b01ae1a84e6fa3b0cbae0271754e4
Pushpam Matah <pushpam.matah@4thpointer.com>
2020-05-14 13:29:12 +0530

    Create reset password module modified

--- src/app/core/authentication/reset-password/reset-password.module.ts
+++ src/app/core/authentication/reset-password/reset-password.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '@app/shared';
 import { ResetPasswordRoutingModule } from './reset-password-routing.module';
 import { ResetPasswordComponent } from './reset-password.component';
 
 @NgModule({
   declarations: [ResetPasswordComponent],
-  imports: [CommonModule, ResetPasswordRoutingModule]
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    SharedModule,
+    ResetPasswordRoutingModule
+  ]
 })
 export class ResetPasswordModule {}
