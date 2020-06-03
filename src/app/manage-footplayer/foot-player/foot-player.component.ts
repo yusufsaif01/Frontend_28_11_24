2149b950fb0e053ff77e1917c4736a3ec598cf49
amit4ps <amit.kumar@4thpointer.com>
2020-06-03 17:27:06 +0530

    FootPlayer Page

--- src/app/manage-footplayer/foot-player/foot-player.component.ts
+++ src/app/manage-footplayer/foot-player/foot-player.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { FootPlayerTableConfig } from './foot-player-table-conf';
+import { environment } from '../../../environments/environment';
+import { PanelOptions } from '@app/shared/models/panel-options.model';
 
 @Component({
   selector: 'app-foot-player',
@@ -6,7 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./foot-player.component.scss']
 })
 export class FootPlayerComponent implements OnInit {
-  constructor() {}
+  // TABLE CONFIG
+  public tableConfig: FootPlayerTableConfig = new FootPlayerTableConfig();
+  public dataSource = new MatTableDataSource([]);
+  pageSize: number = 10;
+  currentPageNo: number = 1;
+  selectedPage: number;
+  environment = environment;
+  player_type: string;
+  member_type: string;
+  show_count: number;
+  total_count: number;
+
+  // LEFT PANEL
+  panelOptions: Partial<PanelOptions> = {
+    bio: true,
+    member_type: true,
+    my_achievements: true,
+    view_profile_link: true,
+    is_public: false
+  };
+  isPublic: boolean = false;
+  userId: string;
 
   ngOnInit() {}
 }
