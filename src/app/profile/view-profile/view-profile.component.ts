7f3eeb94ee95caf423bd002e6929ad6e753a899c
samyakjain12 <samyak.jain@4thpointer.com>
2020-06-26 18:20:57 +0530

    remove employment contract in my_profile

--- src/app/profile/view-profile/view-profile.component.ts
+++ src/app/profile/view-profile/view-profile.component.ts
@@ -25,7 +25,7 @@ export class ViewProfileComponent implements OnInit, OnDestroy {
   aadhar_front_url: string;
   aadhar_back_url: string;
   player_photo_url: string;
-  employment_contract_url: string;
+  // employment_contract_url: string;
   document_url: string;
   document_type: string;
   panelOptions: Partial<PanelOptions> = {
@@ -103,9 +103,9 @@ export class ViewProfileComponent implements OnInit, OnDestroy {
           }
           this.player_photo_url = fileLink + rootMedia.user_photo;
         }
-        if (element.type === 'employment_contract') {
-          this.employment_contract_url = fileLink + rootMedia.document;
-        }
+        // if (element.type === 'employment_contract') {
+        //   this.employment_contract_url = fileLink + rootMedia.document;
+        // }
         if (
           element.type !== 'employment_contract' &&
           element.type !== 'aadhar'
