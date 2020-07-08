686e14dd5fb440f77b5c49252d0e5292e3b59702
Rishabh Sharma <rishabh.sharma@4thpointer.com>
2020-07-08 14:28:40 +0530

    create password screen, text-section common component

---
+++ src/app/core/authentication/text-section/text-section.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-text-section',
+  templateUrl: './text-section.component.html',
+  styleUrls: ['./text-section.component.scss']
+})
+export class TextSectionComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
