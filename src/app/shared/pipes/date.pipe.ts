b3ea0d4f6f472fa75aed1d115eb52b8812ac827d
Rishabh Sharma <rishabh.sharma@4thpointer.com>
2020-07-13 18:24:06 +0530

    minor change

--- src/app/shared/pipes/date.pipe.ts
+++ src/app/shared/pipes/date.pipe.ts
@@ -20,9 +20,7 @@ export class DatePipe implements PipeTransform {
       created_at = minutes + ' min';
     }
     if (minutes >= 60 && hours < 24) {
-      hours == 1
-        ? (created_at = hours + ' hour ago')
-        : (created_at = hours + ' hours ago');
+      created_at = hours == 1 ? hours + ' hour ago' : hours + ' hours ago';
     }
     if (hours >= 24) {
       let finalDate = new DatePipeAngular('en-US');
