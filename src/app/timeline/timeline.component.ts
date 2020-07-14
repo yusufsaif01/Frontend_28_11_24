1d0896cf77f895114752180b5b88576f9bcc4611
amit4ps <amit.kumar@4thpointer.com>
2020-07-14 17:44:17 +0530

    timeline right sections

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -102,13 +102,13 @@ export class TimelineComponent implements OnInit, OnDestroy {
         items: 1
       },
       400: {
-        items: 2
+        items: 1
       },
       740: {
-        items: 3
+        items: 1
       },
       940: {
-        items: 3
+        items: 1
       }
     }
   };
