11a7e20504647edcd428302dc26f76e1fbeecbaa
samyakjain12 <samyak.jain@4thpointer.com>
2020-07-17 18:25:55 +0530

    done view

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,8 +14,8 @@ export const environment = {
   production: false,
   hmr: false,
   version: env.npm_package_version + '-dev',
-  serverUrl: 'http://yftchain-dev.4thpointer.in/api',
-  mediaUrl: 'http://yftchain-dev.4thpointer.in',
+  serverUrl: 'http://yftchain.local/api',
+  mediaUrl: 'http://localhost:4200',
   defaultLanguage: 'en-US',
   supportedLanguages: ['en-US', 'fr-FR']
 };
