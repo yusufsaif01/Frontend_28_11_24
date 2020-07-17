e39e3fcc92a3df211a0b3d2f517e0244c97c8cc2
samyakjain12 <samyak.jain@4thpointer.com>
2020-07-17 18:42:57 +0530

    env.ts restored

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,8 +14,8 @@ export const environment = {
   production: false,
   hmr: false,
   version: env.npm_package_version + '-dev',
-  serverUrl: 'http://yftchain.local/api',
-  mediaUrl: 'http://localhost:4200',
+  serverUrl: 'http://yftchain-dev.4thpointer.in/api',
+  mediaUrl: 'http://yftchain-dev.4thpointer.in',
   defaultLanguage: 'en-US',
   supportedLanguages: ['en-US', 'fr-FR']
 };
