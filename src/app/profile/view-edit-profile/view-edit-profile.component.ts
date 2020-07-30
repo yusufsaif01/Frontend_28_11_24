f37985852ec24a67a16a95e8382b79ac7fd81a9b
Pushpam Matah <pushpam.matah@4thpointer.com>
2020-07-30 16:56:54 +0530

    YFT-1256 modified

--- src/app/profile/view-edit-profile/view-edit-profile.component.ts
+++ src/app/profile/view-edit-profile/view-edit-profile.component.ts
@@ -8,7 +8,6 @@ import { Component, OnInit } from '@angular/core';
 export class ViewEditProfileComponent implements OnInit {
   clubAcademyType: string;
   componentName: string = 'personal';
-  memberType: string = 'player';
   renderComponents: any[];
   components: any[] = [
     { name: 'Personal details', value: 'personal' },
@@ -29,9 +28,8 @@ export class ViewEditProfileComponent implements OnInit {
   }
 
   getMemberType(value: string) {
-    this.memberType = value;
     this.renderComponents = this.components.filter(item => {
-      if (this.memberType !== 'player') return item.value !== 'contracts';
+      if (value !== 'player') return item.value !== 'contracts';
       return item.value;
     });
   }
