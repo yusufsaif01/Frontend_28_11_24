b2d9825609b356a7852940ddd1290bd9f7b4ace2
samyakjain12 <samyak.jain@4thpointer.com>
2020-08-12 15:53:13 +0530

    tab generation

--- src/app/report-card/add-edit-report-card/add-edit-report-card.component.ts
+++ src/app/report-card/add-edit-report-card/add-edit-report-card.component.ts
@@ -5,6 +5,18 @@ import { ToastrService } from 'ngx-toastr';
 import { ActivatedRoute } from '@angular/router';
 import { AddEditEmploymentContractService } from '@app/profile/add-edit-employment-contract/add-edit-employment-contract.service';
 import { untilDestroyed } from '@app/core';
+import { AdminService } from '@app/admin/admin.service';
+import { SharedService } from '@app/shared/shared.service';
+
+interface tab {
+  ability_id: string;
+  ability_name: string;
+  attributes: {
+    attribute_id: string;
+    attribute_name: string;
+    attribute_score?: string;
+  }[];
+}
 
 @Component({
   selector: 'app-add-edit-report-card',
@@ -16,12 +28,25 @@ export class AddEditReportCardComponent implements OnInit, OnDestroy {
   value = 50;
   send_to = '';
   playerDetails: any = {};
-
+  tabs: tab[] = [];
+  currentTab: tab = {
+    ability_id: '',
+    ability_name: '',
+    attributes: [
+      {
+        attribute_id: '',
+        attribute_name: '',
+        attribute_score: ''
+      }
+    ]
+  };
   constructor(
     private _addEditReportCardService: AddEditReportCardService,
     private _employmentContractService: AddEditEmploymentContractService,
+    // private _sharedService: AdminService,
     private _toastrService: ToastrService,
-    private _activatedRoute: ActivatedRoute
+    private _activatedRoute: ActivatedRoute,
+    private _sharedService: SharedService
   ) {
     this.createForm();
     this._activatedRoute.params.subscribe(param => {
@@ -32,7 +57,9 @@ export class AddEditReportCardComponent implements OnInit, OnDestroy {
     });
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getAbilityList();
+  }
 
   getPlayerDetails(player_id: string) {
     this._employmentContractService
@@ -46,6 +73,60 @@ export class AddEditReportCardComponent implements OnInit, OnDestroy {
           this._toastrService.error('Error', error.error.message);
         }
       );
+  }
+
+  getTab(val: string) {
+    this.currentTab = this.tabs.find(tab => {
+      return tab.ability_id === val;
+    });
+  }
+
+  getAbilityList() {
+    this._sharedService
+      .getAbilityList()
+      .pipe(untilDestroyed(this))
+      .subscribe(
+        response => {
+          let records = response.data.records;
+          this.currentTab.ability_id = records[0].id;
+          records.forEach(record => {
+            let tab: tab = {
+              ability_id: record.id,
+              ability_name: record.name,
+              attributes: []
+            };
+            let attribute: any = this.getAttributeListByAbility(record.id);
+            if (attribute && attribute.length) {
+              tab.attributes.push({
+                attribute_id: attribute.id,
+                attribute_name: attribute.name
+              });
+            }
+            this.tabs.push(tab);
+          });
+        },
+        error => {}
+      );
+  }
+
+  getAttributeListByAbility(ability_id: string) {
+    // this.tabs.forEach(tab => {
+    this._sharedService
+      .getAttributeListByAbility({
+        ability_id
+      })
+      .pipe(untilDestroyed(this))
+      .subscribe(
+        response => {
+          return response.data.records;
+          // let tab = {}
+          // tab = response.data
+          // this.abilityName = .ability;
+          // let records = response.data.records;
+        },
+        error => {}
+      );
+    // });
   }
 
   createForm() {}
--- src/app/shared/shared.service.ts
+++ src/app/shared/shared.service.ts
@@ -6,8 +6,39 @@ const routes = {
   getDistrictList: (countryID: any, stateID: any) =>
     `/master/district/list/${countryID}/${stateID}`,
   getStatesList: (countryID: any) => `/master/state/list/${countryID}`,
-  getLocationStats: () => `/master/location/stats`
+  getLocationStats: () => `/master/location/stats`,
+  getAbilityList: () => '/admin/master/player-specialization/ability/list',
+  getAttributeListByAbility: (c: GetAttributeListContext) =>
+    '/master/player-specialization/attribute/list'
 };
+
+interface GetAttributeListContext {
+  ability_id: string;
+}
+interface GetAbilityListResponseContext {
+  status: string;
+  message: string;
+  data: {
+    total: number;
+    records: {
+      id: string;
+      name: string;
+    }[];
+  };
+}
+
+interface GetAttributeListResponseContext {
+  status: string;
+  message: string;
+  data: {
+    ability: string;
+    total: number;
+    records: {
+      id: string;
+      name: string;
+    }[];
+  };
+}
 
 @Injectable({
   providedIn: 'root'
@@ -50,5 +81,23 @@ export class SharedService {
 
   getFilterDisplayValue(): Subject<boolean> {
     return this.displayFilterSubject;
+  }
+
+  getAbilityList(): Observable<GetAbilityListResponseContext> {
+    return this.httpClient.get<GetAbilityListResponseContext>(
+      routes.getAbilityList()
+    );
+  }
+
+  getAttributeListByAbility(
+    context: GetAttributeListContext
+  ): Observable<GetAttributeListResponseContext> {
+    let params = '/';
+    if (context['ability_id']) {
+      params += `${context['ability_id']}`;
+    }
+    return this.httpClient.get<GetAttributeListResponseContext>(
+      routes.getAttributeListByAbility(context) + params
+    );
   }
 }
