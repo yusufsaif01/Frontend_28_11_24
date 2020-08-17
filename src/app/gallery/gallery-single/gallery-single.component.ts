800b3468076cc0a0185eb4fbf35cdea733e9ce59
Pushpam Matah <pushpam.matah@4thpointer.com>
2020-08-17 21:59:47 +0530

    Basic structure made for gallery single component

--- src/app/gallery/gallery-single/gallery-single.component.ts
+++ src/app/gallery/gallery-single/gallery-single.component.ts
@@ -1,12 +1,285 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { environment } from '@env/environment';
 
+import { MatDialog } from '@angular/material/dialog';
+import { PanelOptions } from '@app/shared/models/panel-options.model';
+import { GallerySingleService } from './gallery-single.service';
+import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
+import { untilDestroyed } from '@app/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { wordCount } from '@app/shared/validators/wordCount';
+import { DeleteConfirmationComponent } from '@app/shared/dialog-box/delete-confirmation/delete-confirmation.component';
+
+interface PostContext {
+  id: string;
+  post: {
+    text: string;
+    media_url: string;
+    media_type: string;
+  };
+  posted_by: {
+    avatar: string;
+    member_type: string;
+    user_id: string;
+    name: string;
+    type: string;
+    position: string;
+  };
+  is_liked: boolean;
+  likes: number;
+  comments: {
+    total: number;
+    data: {
+      comment: string;
+      commented_by: {
+        avatar: string;
+        member_type: string;
+        user_id: string;
+        name: string;
+        type: string;
+        position: string;
+      };
+      commented_at: string;
+    }[];
+  };
+  created_at: string;
+  show_comment_box?: boolean;
+  commentListing?: CommentContext[];
+  commentForm?: FormGroup;
+  commentPageNo?: number;
+  commentPageSize?: number;
+  addComment$?: Observable<any>;
+  like$?: Observable<any>;
+}
+
+interface CommentContext {
+  comment: string;
+  commented_by: {
+    avatar: string;
+    member_type: string;
+    user_id: string;
+    name: string;
+    type: string;
+    position: string;
+  };
+  commented_at: string;
+}
 @Component({
   selector: 'app-gallery-single',
   templateUrl: './gallery-single.component.html',
   styleUrls: ['./gallery-single.component.scss']
 })
-export class GallerySingleComponent implements OnInit {
-  constructor() {}
+export class GallerySingleComponent implements OnInit, OnDestroy {
+  constructor(
+    private _gallerySingleService: GallerySingleService,
+    private _toastrService: ToastrService,
+    private _formBuilder: FormBuilder,
+    private dialog: MatDialog
+  ) {}
 
-  ngOnInit() {}
+  environment = environment;
+  postListing: PostContext[] = [];
+  pageNo: number = 1;
+  pageSize: number = 5;
+  panelOptions: Partial<PanelOptions> = {
+    bio: true,
+    member_type: true,
+    my_achievements: true,
+    view_profile_link: true,
+    player_type: true
+  };
+
+  ngOnInit() {
+    this.getPost();
+  }
+
+  addComment(post: PostContext) {
+    post.addComment$ = this._gallerySingleService
+      .addComment({
+        post_id: post.id,
+        ...post.commentForm.value
+      })
+      .pipe(
+        map(resp => {
+          post.commentForm.reset();
+          post.comments.total++;
+          let isViewedMore: boolean = post.commentPageNo > 1;
+          if (isViewedMore) post.commentPageNo = 1;
+          this.getCommentListing(post, false);
+        }),
+        catchError(err => {
+          this._toastrService.error('Error', err.error.message);
+          throw err;
+        }),
+        untilDestroyed(this)
+      );
+  }
+
+  activateCommentBox(post: PostContext) {
+    post.show_comment_box = true;
+  }
+
+  getCommentListing(post: PostContext, shouldLoad?: boolean) {
+    this._gallerySingleService
+      .getCommentListing({
+        page_no: post.commentPageNo,
+        page_size: post.commentPageSize,
+        post_id: post.id
+      })
+      .pipe(untilDestroyed(this))
+      .subscribe(
+        response => {
+          let comments: CommentContext[] = response.data.records;
+          comments.forEach(comment => {
+            if (comment.commented_by.avatar) {
+              comment.commented_by.avatar =
+                environment.mediaUrl + comment.commented_by.avatar;
+            }
+          });
+          if (!shouldLoad) {
+            post.commentListing = comments;
+          } else {
+            post.commentListing.reverse();
+            comments.forEach(comment => {
+              if (!post.commentListing.includes(comment)) {
+                post.commentListing.push(comment);
+              }
+            });
+          }
+          post.commentListing.reverse();
+        },
+        error => {
+          this._toastrService.error('Error', error.error.message);
+        }
+      );
+  }
+
+  toggleLike(post: PostContext) {
+    if (post.is_liked) {
+      post.like$ = this._gallerySingleService
+        .unlikePost({ post_id: post.id })
+        .pipe(
+          map(resp => {
+            post.is_liked = false;
+            post.likes--;
+          }),
+          catchError(err => {
+            this._toastrService.error('Error', err.error.message);
+            throw err;
+          }),
+          untilDestroyed(this)
+        );
+    } else {
+      post.like$ = this._gallerySingleService
+        .likePost({ post_id: post.id })
+        .pipe(
+          map(resp => {
+            post.is_liked = true;
+            post.likes++;
+          }),
+          catchError(err => {
+            this._toastrService.error('Error', err.error.message);
+            throw err;
+          }),
+          untilDestroyed(this)
+        );
+    }
+  }
+
+  createCommentForm(post: PostContext) {
+    post.commentForm = this._formBuilder.group({
+      comment: ['', [Validators.required, wordCount]]
+    });
+  }
+
+  getPost(scrolled?: string) {
+    if (!scrolled) {
+      this.pageNo = 1;
+    }
+    this._gallerySingleService
+      .getPost({
+        comments: 1
+      })
+      .pipe(untilDestroyed(this))
+      .subscribe(
+        response => {
+          let posts: PostContext[] = response.data.records;
+          // this.postCount = response.data.records.length;
+          posts.forEach(post => {
+            if (post.posted_by.avatar) {
+              post.posted_by.avatar =
+                environment.mediaUrl + post.posted_by.avatar;
+            }
+            if (post.post.media_url) {
+              post.post.media_url = environment.mediaUrl + post.post.media_url;
+            }
+            post.commentPageNo = 1;
+            post.commentPageSize = 3;
+            post.commentListing = [];
+            let comments: CommentContext[] = post.comments.data;
+            comments.forEach(comment => {
+              if (comment.commented_by.avatar) {
+                comment.commented_by.avatar =
+                  environment.mediaUrl + comment.commented_by.avatar;
+              }
+            });
+            post.commentListing = comments;
+            post.commentListing.reverse();
+            // this.getCommentListing(post, false, false);
+            let commentForm: FormGroup;
+            post.commentForm = commentForm;
+            this.createCommentForm(post);
+          });
+          if (!scrolled) {
+            this.postListing = posts;
+          } else {
+            posts.forEach(post => {
+              if (!this.postListing.includes(post)) {
+                this.postListing.push(post);
+              }
+            });
+          }
+        },
+        error => {
+          this._toastrService.error('Error', error.error.message);
+        }
+      );
+  }
+
+  deletePost(post_id: string) {
+    const dialogRef = this.dialog.open(DeleteConfirmationComponent, {
+      width: '50% ',
+      panelClass: 'filterDialog',
+      data: {
+        header: 'Delete post',
+        message: 'Are you sure you want to delete?'
+      }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result === true) {
+        this._gallerySingleService
+          .deletePost(post_id)
+          .pipe(untilDestroyed(this))
+          .subscribe(
+            response => {
+              this._toastrService.success(
+                `Success`,
+                'Post deleted successfully'
+              );
+              this.getPost();
+            },
+            error => {
+              this._toastrService.error(
+                `${error.error.message}`,
+                'Delete post'
+              );
+            }
+          );
+      }
+    });
+  }
+
+  ngOnDestroy() {}
 }
---
+++ src/app/gallery/gallery-single/gallery-single.service.ts
@@ -0,0 +1,192 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const routes = {
+  addComment: (params: string) => `/post/${params}/comment`,
+  likePost: (params: string) => `/post/${params}/like`,
+  unlikePost: (params: string) => `/post/${params}/dislike`,
+  getPost: (query: string) => `/posts/list${query}`,
+  updatePost: (post_id: string) => `/post/${post_id}`,
+  deletePost: (post_id: string) => `/post/${post_id}`,
+  getCommentListing: (params: string, query: string) =>
+    `/post/${params}/comments${query}`
+};
+
+interface GetPostListingContext {
+  page_no?: number;
+  page_size?: number;
+  comments?: number;
+}
+
+interface GetCommentListingContext {
+  post_id: string;
+  page_no?: number;
+  page_size?: number;
+}
+
+interface GetCommentListingResponseContext {
+  status: string;
+  message: string;
+  data: {
+    total: number;
+    records: {
+      comment: string;
+      commented_by: {
+        avatar: string;
+        member_type: string;
+        user_id: string;
+        name: string;
+        type: string;
+        position: string;
+      };
+      commented_at: string;
+    }[];
+  };
+}
+
+interface GetPostListingResponseContext {
+  status: string;
+  message: string;
+  data: {
+    total: number;
+    records: {
+      id: string;
+      post: {
+        text: string;
+        media_url: string;
+        media_type: string;
+      };
+      posted_by: {
+        avatar: string;
+        member_type: string;
+        user_id: string;
+        name: string;
+        type: string;
+        position: string;
+      };
+      is_liked: boolean;
+      likes: number;
+      comments: {
+        total: number;
+        data: {
+          comment: string;
+          commented_by: {
+            avatar: string;
+            member_type: string;
+            user_id: string;
+            name: string;
+            type: string;
+            position: string;
+          };
+          commented_at: string;
+        }[];
+      };
+      created_at: string;
+    }[];
+  };
+}
+
+interface CommonResponseContext {
+  status: string;
+  message: string;
+}
+interface likeUnlikeContext {
+  post_id: string;
+}
+
+interface CommonResponseContext {
+  status: string;
+  message: string;
+}
+interface AddCommentContext {
+  post_id: string;
+  comment: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GallerySingleService {
+  constructor(private httpClient: HttpClient) {}
+
+  addComment(context: AddCommentContext): Observable<CommonResponseContext> {
+    let params = '';
+
+    if (context['post_id']) {
+      params += `${context['post_id']}`;
+    }
+
+    let { comment } = context;
+
+    return this.httpClient.post<CommonResponseContext>(
+      routes.addComment(params),
+      { comment }
+    );
+  }
+
+  likePost(context: likeUnlikeContext): Observable<CommonResponseContext> {
+    let params = '';
+    if (context['post_id']) {
+      params += `${context['post_id']}`;
+    }
+    return this.httpClient.post<CommonResponseContext>(
+      routes.likePost(params),
+      context
+    );
+  }
+
+  unlikePost(context: likeUnlikeContext): Observable<CommonResponseContext> {
+    let params = '';
+    if (context['post_id']) {
+      params += `${context['post_id']}`;
+    }
+    return this.httpClient.post<CommonResponseContext>(
+      routes.unlikePost(params),
+      context
+    );
+  }
+
+  updatePost(post_id: string, context: any): Observable<any> {
+    return this.httpClient.put<any>(routes.updatePost(post_id), context);
+  }
+
+  deletePost(post_id: string): Observable<any> {
+    return this.httpClient.delete<any>(routes.deletePost(post_id));
+  }
+
+  getPost(
+    context: GetPostListingContext
+  ): Observable<GetPostListingResponseContext> {
+    let query = '?';
+
+    if (context['comments']) {
+      query += '&comments=' + context['comments'];
+    }
+
+    return this.httpClient.get<GetPostListingResponseContext>(
+      routes.getPost(query)
+    );
+  }
+
+  getCommentListing(
+    context: GetCommentListingContext
+  ): Observable<GetCommentListingResponseContext> {
+    let params = '';
+    if (context['post_id']) {
+      params += `${context['post_id']}`;
+    }
+
+    let query = '?';
+    if (context['page_no']) {
+      query += 'page_no=' + context['page_no'];
+    }
+    if (context['page_size']) {
+      query += '&page_size=' + context['page_size'];
+    }
+
+    return this.httpClient.get<GetCommentListingResponseContext>(
+      routes.getCommentListing(params, query)
+    );
+  }
+}
