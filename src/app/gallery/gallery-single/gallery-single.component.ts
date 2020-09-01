29d626757c5992c3c3c2c5d05ab5b78bb8678d22
akshay_balguhare <akshay.balguhare@4thpointer.com>
2020-09-01 13:06:44 +0530

    removed header from gallery/timeline delete popup

--- src/app/gallery/gallery-single/gallery-single.component.ts
+++ src/app/gallery/gallery-single/gallery-single.component.ts
@@ -294,7 +294,6 @@ export class GallerySingleComponent implements OnInit, OnDestroy {
       width: '50% ',
       panelClass: 'filterDialog',
       data: {
-        header: 'Delete video',
         message: 'Are you sure you want to delete?'
       }
     });
