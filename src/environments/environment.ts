8e2425b4557977dcc97625714460f4732d190187
Yusuf Saif <148246593+yusufsaif01@users.noreply.github.com>
2024-10-03 23:14:21 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,9 +13,8 @@ export const environment = {
   production: false,
   hmr: false,
   version: env.npm_package_version + '-dev',
-  serverUrl: 'http://yftchain.local/api',
-  mediaUrl: 'http://localhost:3000',
-
+  serverUrl: 'https://www.yftchain.com/api',
+  mediaUrl: 'https://www.yftchain.com',
   defaultLanguage: 'en-US',
   supportedLanguages: ['en-US', 'fr-FR']
 };
